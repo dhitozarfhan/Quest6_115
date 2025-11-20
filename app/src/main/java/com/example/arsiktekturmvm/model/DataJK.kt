6d4fc476df1d46c802d8729420f25b36aa26855e
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-20 16:36:22 +0700

    menambahkan dataJK

---
+++ app/src/main/java/com/example/arsiktekturmvm/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.arsiktekturmvm.model
+
+import com.example.arsiktekturmvm.R
+
+object DataJK {
+    val jenisK= listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
