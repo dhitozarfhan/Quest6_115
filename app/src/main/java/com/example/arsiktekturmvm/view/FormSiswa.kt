5fb5e0446b34960181a8ef2c65dd4a54d7f2da10
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-20 17:34:37 +0700

    menambakan formsiswa

--- app/src/main/java/com/example/arsiktekturmvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/arsiktekturmvm/view/FormSiswa.kt
@@ -82,8 +82,9 @@ fun FormSiswa(
                         item->
                     Row(verticalAlignment = Alignment.CenterVertically){
                         RadioButton(
-                            selected = false,
-                            onClick = {item}
+                            selected =  txtGender == item,
+                            onClick = {
+                                txtGender=item}
                         )
                         Text(text = item)
                     }
