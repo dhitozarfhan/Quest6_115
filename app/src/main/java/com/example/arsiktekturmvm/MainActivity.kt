414ef8d329a7e61cb181c90ea334ea797175d8f6
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-20 17:35:30 +0700

    merubah main activity

--- app/src/main/java/com/example/arsiktekturmvm/MainActivity.kt
+++ app/src/main/java/com/example/arsiktekturmvm/MainActivity.kt
@@ -1,47 +1,27 @@
-package com.example.arsiktekturmvm
+package com.example.praktikumarsitekturvvm
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.arsiktekturmvm.ui.theme.ArsiktekturMVMTheme
+import com.example.arsitekturvm.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ArsiktekturMVMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SiswaApp(modifier = Modifier)// Panggil Composable Navigasi Utama
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ArsiktekturMVMTheme {
-        Greeting("Android")
     }
 }
